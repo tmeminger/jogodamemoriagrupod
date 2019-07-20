30d011aa682f2590daf7da86fefd994689dc5f96
Obede <obede.silva7@gmail.com>
2019-07-20 16:42:29 -0300

    função para armazenar as imgs

--- script.js
+++ script.js
@@ -1,25 +1,26 @@
 //criando lista com todas as 4 divs que criamos no html
-let quadradinhos = document.querySelectorAll(".cards div");
+let quadradinhos = document.querySelectorAll('img');
 
-//criando vetor para guardarmos os nomes das classes 
-let posicaoCores = [];
-
-//percorrendo a lista das divs 
-//e comparando se há uma classe ou não 
-//e inserindo o nome da classe no indice do vetor de acordo com a posição da classe na lista de divs
-for (let index = 0; index < quadradinhos.length; index++) {
-    if (quadradinhos[index].classList.contains("vermelho")) {
-        posicaoCores.push("vermelho");
-    } else {
-        posicaoCores.push("azul");
-    }   
+// Puxar posição e localização das imgs e armarzenar em um vetor
+function pegarCartas(quadradinhos){
+    //criando vetor para guardarmos os nomes das imgs
+    let cartas = [];
+    for (i = 0; i<quadradinhos.length;i++){
+        cartas[i] = quadradinhos[i].src;
+    }
+    // retorna um vetor com os nomes das imgs
+    return cartas;
 }
+// armazendo os nomes da imgs e suas posições em uma variavel
+cartas = pegarCartas(quadradinhos);
+
 
 //imprimindo no console o vetor para verificar se o passo anterior funcionou corretamente
-console.log(posicaoCores);
+console.log(cartas);
 
-//definindo o tempo de 3seg para que as cores apareçam para o user e depois as cartas se tornem pretas
+
 setTimeout(function corBg(){
+    //definindo o tempo de 3seg para que as cores apareçam para o user e depois as cartas se tornem pretas
     for (let qd of quadradinhos) {
     qd.classList.remove("vermelho");
     qd.classList.remove("azul");
@@ -29,7 +30,7 @@ setTimeout(function corBg(){
 //FALTA - definir a função para clique
 //para comparar
 function mudarCor(event){
-    if(event.target.style)
+    if(event.target.style){}
 }
 
 for(let quadradinho of quadradinhos){
